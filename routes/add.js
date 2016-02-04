322ee4d618db7ec967bc02cff19cf21f684c0dee
Diana Ho <dianahox3@gmail.com>
2016-02-04 20:14:29 +0000

    Lab 5 sucks

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,6 @@
 var data = require("../data.json");
 
-exports.addFriend = function(req, res) {    
-	// Your code goes here
- }
+exports.addFriend = function(req, res) {
+	console.log(data);
+	res.render('add', data);
+}
