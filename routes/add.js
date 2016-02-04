876c84d1ef7f7287615992200c82fc0cfc163e13
Diana Ho <dianahox3@gmail.com>
2016-02-04 20:47:17 +0000

    Finishing up

--- routes/add.js
+++ routes/add.js
@@ -1,6 +1,15 @@
 var data = require("../data.json");
 
 exports.addFriend = function(req, res) {
-	console.log(data);
-	res.render('add', data);
+	var newName = req.query.name;
+	var newDesc = req.query.description;
+
+	var newFriend = {
+		"name": newName,
+		"description": newDesc,
+		"imageURL": "http://lorempixel.com/400/400"
+	}
+
+	res.render('add', newFriend);
+	data["friends"].push(newFriend);
 }
